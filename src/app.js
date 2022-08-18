a4bdfea07d6ff150d0c9f59bc08eae358a10cf5d
Pierre Muller <pierremanu.muller@gmail.com>
2022-08-18 16:37:10 +0200

    feat(app): /register and /login routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,84 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const User = require("./model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
+
+// Routes
+app.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All inputs are required");
+    }
+
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User already exists. Please log in");
+    }
+
+    encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(),
+      password: encryptedUserPassword,
+    });
+
+    const token = jwt.sign(
+      {
+        user_id: user.id,
+        email,
+      },
+      process.env.TOKEN_KEY,
+      { expiresIn: "5h" }
+    );
+    user.token = token;
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All inputs are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        {
+          user_id: user.id,
+          email,
+        },
+        process.env.TOKEN_KEY,
+        { expiresIn: "5h" }
+      );
+
+      user.token = token;
+
+      return res.status(200).json(user);
+    }
+
+    return res.status(400).send("Invalid credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = app;
