dffeeb63c907a95450b44e596f1f25caf07c8c07
Pierre Muller <pierremanu.muller@gmail.com>
2022-08-18 17:05:58 +0200

    feat(app): enable cors
    also 404 route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const cors = require("cors");
 const auth = require("./middleware/auth");
 const User = require("./model/user");
 const bcrypt = require("bcryptjs");
@@ -9,7 +10,8 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 // Middlewares
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
 // Routes
 app.get("/welcome", auth, (req, res) => {
@@ -84,6 +86,17 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
 });
 
 module.exports = app;
